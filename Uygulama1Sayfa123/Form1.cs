d5acd5fa03a9885858fcb3ee16ae72335dfd7f1d
efe33 <efe33@BPC04>
2024-01-09 09:14:46 +0300

    Proje dosyası ekle.

---
+++ Uygulama1Sayfa123/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama1Sayfa123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string[] isimler = new string[5];
+        int index = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+           isimler[index]= txtAdSoyad.Text;
+            index ++;
+            
+        }
+    }
+}
